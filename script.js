b61de3909f3f3bd33ccda78936982249a1bc0f84
Sumit Desale <sumitdesale706@gmail.com>
2026-09-25 00:16:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,87 @@
 //your JS code here. If required.
+let body = document.querySelector("body");
+
+let heading = document.createElement("h1");
+heading.innerText = "MyBookList";
+body.appendChild(heading);
+
+
+// for form :-
+let form = document.createElement("div");
+form.classList.add("form-group");
+
+let title = document.createElement("input");
+title.type = "text";
+title.id = "title";
+
+let author = document.createElement("input");
+author.type = "text";
+author.id = "author";
+
+let isbn = document.createElement("input");
+isbn.type = "number";
+isbn.id = "isbn";
+
+let button = document.createElement("button");
+button.id = "submit";
+button.innerText = "Add Book";
+
+form.appendChild(title, author, isbn, button);
+
+
+
+// for table :-
+let table = document.createElement("table");
+table.classList.add("table");
+table.classList.add("table-striped");
+let tHead = document.createElement("thead");
+let headRow = document.createElement("tr");
+let c1  =  document.createElement("td");
+let c2  =  document.createElement("td");
+let c3  =  document.createElement("td");
+c1.innerText = "Title";
+c2.innerText = "Author";
+c3.innerText = "ISBN#";
+c3.colspan = "2";
+headRow.append(c1, c2, c3);
+tHead.appendChild(headRow);
+table.appendChild(tHead);
+
+let tBody = document.createElement("tbody");
+tBody.id = "book-list";
+table.appendChild(tBody);
+
+
+
+// add our form and table in body
+body.appendChild(form, table);
+
+
+button.addEventListener("click", () => {
+	let row = document.createElement("tr");
+	row.id = "r1";
+	
+	let col1 = document.createElement("td");
+	let col2 = document.createElement("td");
+	let col3 = document.createElement("td");
+	let col4 = document.createElement("td");
+
+	col1.innerText = `${title.value}`;
+	col2.innerText = `${author.value}`;
+	col3.innerText = `${}`;
+	let deleteB = document.createElement("button");
+	deleteB.classList.add("delete");
+    col4.appendChild(deleteB);
+
+	row.append(col1, col2, col3, col4);
+	tBody.appendChild(row);
+
+	deleteB.addEventListener("click", () => {
+		   row.remove();
+	})
+    
+})
+
+
+
+
